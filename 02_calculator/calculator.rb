e361270a93410ad5aaa0d865c2191d2542a435d8
Arman <arman.gh45@gmail.com>
2014-04-29 17:09:07 -0400

    First commit

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,19 @@
+def add(num_1, num_2)
+	return num_1 + num_2
+end
+def subtract(num_1, num_2)
+	return num_1 - num_2
+end
+def multiply(num_1, num_2)
+	return num_1 * num_2
+end
+def divide(num_1, num_2)
+	return num_1 / num_2
+end
+def sum(lst)
+	sum = 0
+	for e in lst
+		sum += e	
+	end
+	return sum
+end
